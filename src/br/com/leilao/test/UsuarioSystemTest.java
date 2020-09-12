6a0fe3c20d06a1ab4699f1af74cb7a1bb234a5be
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-09-12 15:57:19 -0300

    Implantado padrão PageObject

--- src/br/com/leilao/test/UsuarioSystemTest.java
+++ src/br/com/leilao/test/UsuarioSystemTest.java
@@ -6,14 +6,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class UsuarioSystemTest {
 
 	private WebDriver driver;
+	private UsuariosPage usuariosPage;
 
 	@BeforeClass
 	public static void iniciaClasse() {
@@ -22,10 +21,8 @@ public class UsuarioSystemTest {
 	
 	@Before
 	public void iniciaMetodo() {
-		System.setProperty("webdriver.chrome.driver","C:\\ChromeDriver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/usuarios");
-		driver.findElement(By.linkText("Novo Usuário")).click();
+		usuariosPage = new UsuariosPage(driver);
 	}
 	
 	@After
@@ -35,51 +32,29 @@ public class UsuarioSystemTest {
 	
 	@Test
 	public void deveAdicionarUmUsuario() {
-		WebElement nome = driver.findElement(By.name("usuario.nome"));
-		WebElement email = driver.findElement(By.name("usuario.email"));
-		
-		nome.sendKeys("Jandira");
-		email.sendKeys("jandira@email.com");
-		
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		botaoSalvar.click();
-		
-		boolean achouNome = driver.getPageSource().contains("Jandira");
-		boolean achouEmail = driver.getPageSource().contains("jandira@email.com");
-
-		assertTrue(achouNome);
-		assertTrue(achouEmail);
+		usuariosPage.visita();
+		usuariosPage.novo().cadastra("Jandira", "jandira@email.com");
+		assertTrue(usuariosPage.existeNaListagem("Jandira", "jandira@email.com"));
 	}
 	
 	@Test
 	public void deveExigirNomeDeUsuario() {
-		WebElement email = driver.findElement(By.name("usuario.email"));
-		
-		email.sendKeys("jandira@email.com");
-		
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		botaoSalvar.click();
-		
-		boolean exigiNome = driver.getPageSource().contains("Nome obrigatorio!");
-		boolean achouEmail = driver.getPageSource().contains("jandira@email.com");
-
-		assertTrue(exigiNome);
-		assertTrue(achouEmail);
+		usuariosPage.visita();
+		usuariosPage.novo().cadastra("", "jandira@email.com");
+		assertTrue(usuariosPage.existeMensagemCampoFaltante("Nome obrigatorio!"));
 	}
 	
 	@Test
 	public void deveExigirEmailDeUsuario() {
-		WebElement nome = driver.findElement(By.name("usuario.nome"));
-		
-		nome.sendKeys("Jandira");
-		
-		WebElement botaoSalvar = driver.findElement(By.id("btnSalvar"));
-		botaoSalvar.click();
-		
-		boolean exigiEmail = driver.getPageSource().contains("E-mail obrigatorio!");
-		boolean achouNome = driver.getPageSource().contains("Jandira");
-
-		assertTrue(exigiEmail);
-		assertTrue(achouNome);
+		usuariosPage.visita();
+		usuariosPage.novo().cadastra("Jandira", "");
+		assertTrue(usuariosPage.existeMensagemCampoFaltante("E-mail obrigatorio!"));
+	}
+	
+	@Test
+	public void deveExcluirUsuario() {
+		usuariosPage.visita();
+		usuariosPage.novo().cadastra("Jandira", "jandira@email.com");
+		usuariosPage.deletaUsuariosNaPosicao(0);
 	}
 }
---
+++ src/br/com/leilao/test/UsuariosPage.java
@@ -0,0 +1,37 @@
+package br.com.leilao.test;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class UsuariosPage {
+
+	private WebDriver driver;
+
+	UsuariosPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	void visita() {
+		driver.get("http://localhost:8080/usuarios");
+	}
+	
+	NovoUsuarioPage novo() {
+		driver.findElement(By.linkText("Novo Usuário")).click();
+		return new NovoUsuarioPage(driver);
+	}
+	
+	Boolean existeNaListagem(String nome, String email) {
+		return driver.getPageSource().contains(nome) && driver.getPageSource().contains(email);
+	}
+	
+	Boolean existeMensagemCampoFaltante(String mensagem) {
+		return driver.getPageSource().contains(mensagem);
+	}
+	
+	void deletaUsuariosNaPosicao(Integer posicao) {
+		driver.findElements(By.tagName("button")).get(posicao).click();
+		Alert alert = driver.switchTo().alert();
+		alert.accept();
+	}
+}
